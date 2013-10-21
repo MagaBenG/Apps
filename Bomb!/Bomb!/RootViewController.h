8a0e683459947858c02749ed110f59aa4016b1fb
MagaBenG <ben--g2@hotmail.com>
2013-10-20 21:00:21 -0400

    small changes because i couldent make up my mind

---
+++ Bomb!/Bomb!/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Bomb!
+//
+//  Created by Benjamin DosSantos on 10/20/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
